ef4f33ab3a144904dc1c2663276b42b0194a20bf
Aleaume Landrea <51741094+landreaa@users.noreply.github.com>
2023-01-11 17:32:03 +0100

    added cameras

---
+++ glimac/glimac/FreeFlyCamera.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <vector>
+#include "common.hpp"
+#include "glm.hpp"
+
+namespace glimac {
+
+// Représente une camera qui cible un point précis
+
+    class FreeFlyCamera {
+    public:
+        // Constructeur: alloue le tableau de données et construit les attributs des vertex
+        FreeFlyCamera()
+        {
+            this->m_Position = glm::vec3(0.f);
+            this->m_fPhi = glm::pi<float>();
+            this->m_fTheta = 0.f;
+        }
+
+        void computeDirectionVectors()
+        {
+            float radPhi = m_fPhi;
+            float radTheta = m_fTheta;
+            m_FrontVector  = glm::vec3(glm::cos(radTheta) * glm::sin(radPhi), glm::sin(radTheta), glm::cos(radTheta) * glm::cos(radPhi));
+            m_LeftVector   = glm::vec3(glm::sin(radPhi + (glm::pi<float>() / 2.f)), 0, glm::cos(radPhi + (glm::pi<float>() / 2.f)));
+            m_UpVector     = glm::cross(m_FrontVector, m_LeftVector);
+        }
+
+        void moveLeft(float t){
+            m_Position += t * m_LeftVector;
+            computeDirectionVectors();
+        }
+
+        /// @brief Permet d'avancer / reculer la caméra de la distance delta (t positif avance la camera)
+        /// @param t
+        void moveFront(float t){
+            m_Position += t * m_FrontVector;
+            computeDirectionVectors();
+        }
+
+        /// @brief Permet de tourner horizontalement autour du centre de vision
+        /// @param degrees
+        void rotateLeft(float degrees)
+        {
+            m_fPhi += glm::radians(degrees);
+            computeDirectionVectors();
+        }
+
+        /// @brief Permet de tourner verticalement autour du centre de vision
+        /// @param degrees
+        void rotateUp(float degrees)
+        {
+            m_fTheta += glm::radians(degrees);
+            computeDirectionVectors();
+        }
+
+        glm::mat4 getViewMatrix() const
+        {
+            glm::mat4 ViewMatrix = glm::lookAt(m_Position, m_Position + m_FrontVector, m_UpVector);
+            return ViewMatrix;
+        }
+
+    private:
+        glm::vec3 m_Position; // position camera
+        float m_fPhi; // angle autour de l'axe x (haut et bas)
+        float m_fTheta; // angle autour de l'axe y (gauche et droite)
+
+        glm::vec3 m_FrontVector;
+        glm::vec3 m_LeftVector;
+        glm::vec3 m_UpVector;
+    };
+
+} // namespace glimac
---
+++ glimac/glimac/TrackballCamera.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <vector>
+#include "common.hpp"
+#include "glm.hpp"
+
+namespace glimac {
+
+// Représente une camera qui cible un point précis
+
+    class TrackballCamera {
+    public:
+        // Constructeur: alloue le tableau de données et construit les attributs des vertex
+        TrackballCamera()
+        {
+            this->m_fAngleX = 0.f;
+            this->m_fAngleY = 0.f;
+            this->m_fDistance = 5.f;
+        }
+
+        float getAngleX()
+        {
+            return m_fAngleX;
+        }
+
+        float getAngleY()
+        {
+            return m_fAngleY;
+        }
+
+        float getDistance()
+        {
+            return m_fDistance;
+        }
+
+        /// @brief Permet d'avancer / reculer la caméra de la distance delta (delta positif avance la camera)
+        /// @param delta
+        void moveFront(float delta)
+        {
+            m_fDistance -= delta;
+        }
+
+        /// @brief Permet de tourner horizontalement autour du centre de vision
+        /// @param degrees
+        void rotateLeft(float degrees)
+        {
+            m_fAngleX = glm::radians(degrees);
+        }
+
+        /// @brief Permet de tourner verticalement autour du centre de vision
+        /// @param degrees
+        void rotateUp(float degrees)
+        {
+            m_fAngleY = glm::radians(degrees);
+        }
+
+        glm::mat4 getViewMatrix() const
+        {
+            glm::mat4 ViewMatrix = glm::mat4(1);
+            glm::mat4 MoveFrontMatrix = glm::translate(ViewMatrix, glm::vec3(0, 0, -m_fDistance));
+            glm::mat4 RotateLeftMatrix = glm::rotate(ViewMatrix, m_fAngleX, glm::vec3(1, 0, 0));
+            glm::mat4 RotateUpMatrix   = glm::rotate(ViewMatrix, m_fAngleY, glm::vec3(0, 1, 0));
+            return MoveFrontMatrix * RotateLeftMatrix * RotateUpMatrix;
+        }
+
+    private:
+        float m_fDistance; // distance au centre
+        float m_fAngleX; // angle autour de l'axe x (haut et bas)
+        float m_fAngleY; // angle autour de l'axe y (gauche et droite)
+    };
+
+} // namespace glimac
